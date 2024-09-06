cca802eb6c0f55d2a8ec5867ecd82afc3e154b02
Kalki <reddy29@kali>
2024-09-06 07:29:36 +0530

    Java appliaction to retreive songs with their name, language etc,, Also use of stream api (filter and collect).

---
+++ Music.java
@@ -0,0 +1,57 @@
+public class Music {
+    private String name;
+    private String movie;
+    private String artist;
+    private String language;
+    private int year;
+    
+    Music(){
+
+    }
+    //Constructor with parameters
+    public Music(String name, String movie, String artist, String language, int year) {
+        this.name = name;
+        this.movie = movie;
+        this.artist = artist;
+        this.language = language;
+        this.year = year;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMovie() {
+        return movie;
+    }
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+    public String getLanguage() {
+        return language;
+    }
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    @Override
+    public String toString() {
+        return "Music [name=" + name + ", movie=" + movie + ", artist=" + artist + ", language=" + language + ", year="
+                + year + "]";
+    }
+
+      
+    
+}
